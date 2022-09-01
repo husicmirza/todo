28d1a585ccdee4787b00386840c2f5174d8916db
Mirza Husic <mirza.huc@gmail.com>
2022-09-01 16:41:47 +0200

    created 2 buttons  with functionality

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Todo } from "../model";
+import { MdDone } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <form className="todos-single">
+      {todo.completed ? (
+        <s className="todos-single-text"> {todo.title}</s>
+      ) : (
+        <span className="todos-single-text">{todo.title}</span>
+      )}
+      <div>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
