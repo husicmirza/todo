9e81409849e9d4946f78921f6ac8d14ec98f4fd6
Mirza Husic <mirza.huc@gmail.com>
2022-09-01 16:39:35 +0200

    fatch 5 json objects from api

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 import "./styles.css";
 
 interface Props {
@@ -19,6 +20,14 @@ const TodoList: React.FC<Props> = ({
     <div className="container">
       <div className="todos">
         <span className="todos-heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            key={todo.id}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
       </div>
       <div className="todos remove">
         <span className="todos-heading">Compelated Tasks</span>
